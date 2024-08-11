17e0f8e28de3f26df92d847cd2fb4717b6570789
Will <36839175+will-rosenberg@users.noreply.github.com>
2024-08-11 15:00:19 -0500

    Updating to included Ben's updates

--- script.js
+++ script.js
@@ -1,58 +1,149 @@
-//import json files (transcripts including alignment data)
+//// import json files (transcripts including alignment data)
+//// this async method is working better for me than the import method
+let speech1;
+await fetch("./JSON/speech1.json")
+  .then((response) => response.json())
+  .then((json) => (speech1 = json));
 
-import speech1 from './JSON/speech1.json' with {type: 'json'};
-import speech2 from './JSON/speech2.json' with {type: 'json'};
-import speech3 from './JSON/speech3.json' with {type: 'json'};
-// import { speech1, speech2, speech3 } from './app.js';
-// separate js file no longer needed
+//// DOM selection and associated variables
+const audioPlayer = document.getElementById("audioPlayer");
+const audioSource = document.getElementById("audioSource");
+const transcriptDiv = document.getElementById("transcript");
+const toolTip = document.getElementById("toolTip");
 
-// DOM selection and associated variables
-const audioPlayer = document.getElementById('audioPlayer');
-const audioSource = document.getElementById('audioSource');
-const transcriptDiv = document.getElementById('transcript');
+//// variable declarations
 
-// variable declarations
-
-const speechOptions = [1, 2, 3];
-let speechIndex = 3;
-
-const audioURLSelected = `./audio/audio${speechIndex}.mp3`
-const transcriptSelected = speech3.speech.transcripts;
-
-// function definitions
-
-function showCurrentWord (e) {
-    const { duration, currentTime } = e.srcElement
-    console.log(currentTime);
+let inProgress = {
+  // an object containing user-generated edits
+  // could be developed into an output
+  timeIntervals: [],
+  words: [],
+  notes: [],
 };
 
-// main logic
+let issueObj = {
+  // an object used to convert between keypresses and pronunciation issues
+  // just an idea. Not sure how many unique labels are needed / if this approach is enough
+  KeyA: "Too Brazilian",
+  KeyB: "Not Brazilian Enough",
+  KeyC: "Поссиблы Руссиан",
+  KeyD: "Lorem Ipsum",
+  KeyE: "Baa'rekde",
+};
+
+let activeWord = 0; // index of the word currently being spoken
+
+const speechOptions = [1, 2, 3];
+let speechIndex = speechOptions[0];
+
+const audioURLSelected = `./audio/audio${speechIndex}.mp3`;
+const transcriptSelected = speech1.speech.transcripts;
+
+//// function definitions
+function showCurrentWord() {
+  // highlights the word currently being spoken
+  let currentTime = audioPlayer.currentTime;
+  let wordIndex = 0;
+  for (let i = 0; i < inProgress.timeIntervals.length; i++) {
+    if (currentTime > inProgress.timeIntervals[i]) {
+      wordIndex++;
+    } else {
+      break;
+    }
+  }
+  if (wordIndex != activeWord) {
+    activeWord = wordIndex;
+    document.querySelectorAll("span").forEach((sp) => {
+      sp.classList.remove("active");
+    });
+    document.getElementById(`word${activeWord - 1}`).classList.add("active");
+  }
+}
+
+function showAnnotations(ind) {
+  // displays any annotations applied to the word being hovered over
+  document.getElementById("toolTip").innerHTML =
+    inProgress.notes[ind].join(", ");
+}
+
+//// main logic
 // Set audio source
 audioSource.src = audioURLSelected;
 audioPlayer.load();
 
-transcriptSelected.forEach(tranData => {
-    const start_offset = tranData.start_offset;
-    const start_offset_conv = start_offset / 16000;
-    const alignment = tranData.alignment;
+transcriptSelected.forEach((tranData) => {
+  const start_offset = tranData.start_offset;
+  const start_offset_conv = start_offset / 16000;
+  const alignment = tranData.alignment;
 
-    alignment.forEach(wordData => {
-        const wordSpan = document.createElement('span');
-        wordSpan.textContent = wordData.word + ' ';
-        wordSpan.dataset.startTime = wordData.start + start_offset_conv;
+  alignment.forEach((wordData) => {
+    const wordSpan = document.createElement("span");
+    wordSpan.setAttribute("id", `word${inProgress.words.length}`);
+    wordSpan.textContent = wordData.word;
+    wordSpan.dataset.startTime = wordData.start + start_offset_conv;
 
-        wordSpan.addEventListener('click', () => {
-            audioPlayer.currentTime = wordData.start + start_offset_conv;
-            audioPlayer.play();
-        });
-        transcriptDiv.appendChild(wordSpan);
+    //populating inProgress object
+    inProgress.timeIntervals.push(wordData.start + start_offset_conv);
+    inProgress.words.push(wordData.word);
+    inProgress.notes.push([]);
+
+    wordSpan.addEventListener("click", () => {
+      audioPlayer.currentTime = wordData.start + start_offset_conv;
+      audioPlayer.play();
     });
-    const para = document.createElement('p');
-    transcriptDiv.appendChild(para);
+    transcriptDiv.appendChild(wordSpan);
+    transcriptDiv.append("\x20");
+  });
+  const para = document.createElement("p");
+  transcriptDiv.appendChild(para);
 });
 
-// event listeners
-audioPlayer.addEventListener('timeupdate', showCurrentWord);
+//// event listeners
 
-// console.log(wordSpan);
-// console.log(wordData);
+// equivalent but runs a bit faster and more reliably than 'timeupdate'
+// the last argument is the time (ms) between calls of showCurrentWord()
+// could definitely be slowed down a bit if it becomes a performance issue
+setInterval(function () {
+  showCurrentWord();
+}, 30);
+
+// detects hotkey-entered annotations for highlighted span
+// see issueObj for conversions
+document.addEventListener("keydown", (e) => {
+  let hov = document.querySelector("span:hover");
+  if (hov) {
+    hov.classList.add("annotated");
+    let code = e.code;
+    let ind = parseInt(hov.id.slice(4));
+    let notes = inProgress.notes[ind];
+    if (Object.keys(issueObj).includes(code)) {
+      code = issueObj[code];
+    }
+    if (notes.includes(code)) {
+      notes.splice(notes.indexOf(code), 1);
+      if (notes.length == 0) {
+        hov.classList.remove("annotated");
+      }
+    } else {
+      notes.push(code);
+    }
+    showAnnotations(ind);
+  }
+});
+
+// displays annotations in upper right box
+// not a huge fan of the fixed box as the format for this
+// maybe it could be more like a tooltip?
+// better approach will depend on whether it needs to be mobile friendly
+for (let i = 0; i < inProgress.notes.length; i++) {
+  let s = document.querySelectorAll("span")[i];
+  s.addEventListener(
+    "mouseover",
+    (f) => {
+      let ind = parseInt(s.id.slice(4));
+      showAnnotations(ind);
+      console.log(inProgress.words[ind]);
+    },
+    false
+  );
+}
