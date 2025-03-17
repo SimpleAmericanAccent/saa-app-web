3760071ac8c7c221fecab33220a98ead58adc79c
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-03-17 18:26:07 -0500

    Update NavBar.jsx

--- web/frontend/src/components/navbar/NavBar.jsx
+++ web/frontend/src/components/navbar/NavBar.jsx
@@ -6,8 +6,6 @@ function NavBar() {
     <div className="navbar">
       <a href="/logout">Log Out</a>
       {" | "}
-      <Link to="/Home0">0</Link>
-      {" | "}
       <Link to="/Home1">Transcript Viewer</Link>
       {" | "}
       <Link to="/Home6">Stats</Link>
@@ -23,15 +21,6 @@ function NavBar() {
       <Link to="/path">Path</Link>
       {" | "}
       <Link to="/quiz">Quiz</Link>
-      <br />
-      {"Legacy: "}
-      <a href="/home.html">Home</a>
-      {" | "}
-      <a href="/learn2.html">Word Lists & Spelling</a>
-      {" | "}
-      <a href="/learn3.html">Vowel Sounds</a>
-      {" | "}
-      <a href="/synth.html">Vowel Synth</a>
     </div>
   );
 }
