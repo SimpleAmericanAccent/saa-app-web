3e597aa314b986a1f806b6138aec334ca9ebac00
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-03-17 18:58:07 -0500

    typo & some route adjustments

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -9,27 +9,26 @@ import Home5 from "./pages/Home5.jsx";
 import Home6 from "./pages/Home6.jsx";
 import SuccessPath from "./pages/SuccessPath.jsx";
 import Quiz from "./pages/Quiz.jsx";
-import NavBar from "./components/navbar/NavBar.jsx";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home0 />} />
-        <Route path="/home0" element={<Home0 />} />
+        <Route path="/" element={<Home1 />} />
         <Route path="/home1" element={<Home1 />} />
         <Route path="/home2" element={<Home2 />} />
         <Route path="/home3" element={<Home3 />} />
         <Route path="/home5" element={<Home5 />} />
         <Route path="/home6" element={<Home6 />} />
-        <Route path="*" element={<Home0 />} />
+        <Route path="*" element={<Home1 />} />
 
         {/* Protected routes for write access */}
         <Route
           path="/home4"
           element={
-            <ProtectedRoute requiredRole="wee">
+            <ProtectedRoute requiredRole="write">
               <Home4 />
             </ProtectedRoute>
           }
