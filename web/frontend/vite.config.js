229da88db2606f14ce275df499e9afe0e062a78e
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-03-18 14:08:07 -0500

    Update vite.config.js
    don't build sourcemap

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
   server: {
     port: 5173,
     proxy: {
