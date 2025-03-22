512288331205fbc2c8dd57455f58834f65e2cd1b
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-03-22 13:39:10 -0500

    uninstall ag-grid and tanstack react-table and start trying resizable

--- web/frontend/src/components/layout1.jsx
+++ web/frontend/src/components/layout1.jsx
@@ -1,17 +1,25 @@
 import NavBar from "@/components/NavBar.jsx";
 import { SidebarRight } from "@/components/sidebar-right";
 import { Outlet } from "react-router";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Layout1({ children }) {
   return (
     <div className="w-screen h-screen flex flex-col">
-      <NavBar showSidebarTrigger="true" />
-      <div className="flex flex1">
-        <main className="flex1">
-          <Outlet />
-        </main>
-        <SidebarRight className="h-full" />
-      </div>
+      <NavBar />
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel>
+          <main className="flex1">
+            <Outlet />
+          </main>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel></ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
