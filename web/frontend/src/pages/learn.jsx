ec837be0a5b03786feabc84c18e4e5d4ff057a80
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-04-03 13:41:46 -0500

    Update learn.jsx

--- web/frontend/src/pages/learn.jsx
+++ web/frontend/src/pages/learn.jsx
@@ -37,9 +37,26 @@ export default function Learn() {
         </li>
       </ul>
 
+      <p className="mt-4">
+        Here are some of my favorite accent training exercises:
+      </p>
+
+      <ul className="px-8 list-disc">
+        <li>
+          <a
+            href="https://www.loom.com/share/4331ad9391924c219659dbbea578d9cd?sid=0089b33e-4f45-43a6-b7b4-ad8a0817adb0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Some of my favorite exercises for accent training
+          </a>{" "}
+          (with demonstrations and explanations)
+        </li>
+      </ul>
+
       <h1 className="text-2xl font-bold mt-10">External Resources</h1>
 
-      <p className="mt-4">Pracice phrases & words</p>
+      <p className="mt-4">Practice phrases & words</p>
       <ul className="px-8 list-disc">
         <li>
           <a
@@ -72,6 +89,102 @@ export default function Learn() {
           - Use ChatGPT to generate your own practice phrases. I've included a
           sample prompt for you to get started - click to try it out
           automatically. Try following up with "the this that these those"
+        </li>
+      </ul>
+
+      <p className="mt-4">Search For Native Audio</p>
+      <ul className="px-8 list-disc">
+        <li>
+          <a
+            href="https://youglish.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouGlish
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://forvo.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Forvo
+          </a>
+        </li>
+      </ul>
+
+      <p className="mt-4">Software to help you practice/analyze accents</p>
+      <ul className="px-8 list-disc">
+        <li>
+          <a
+            href="https://www.youtube.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube
+          </a>{" "}
+          - Sometimes simple is best! Try using keyboard shortcuts to change
+          speed and jump back and forth.
+        </li>
+        <li>
+          <a
+            href="https://agrahn.gitlab.io/ABLoopPlayer/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AB Loop Player
+          </a>{" "}
+          - Infinitely loop any section of any YouTube video, at any speed you
+          want.
+        </li>
+        <li>
+          <a
+            href="https://www.audacityteam.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Audacity
+          </a>{" "}
+          - If you want to record your own voice / analyze audio more deeply.{" "}
+          <ul className="px-8 list-disc">
+            <li>
+              Here's an old training I did on{" "}
+              <a
+                href="https://www.youtube.com/watch?v=MS3xQ1TBY8c/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                how to use Audacity for accent training.
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a
+            href="https://www.fon.hum.uva.nl/praat/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Praat
+          </a>{" "}
+          - The website looks terrible but Praat is heavily used in
+          academic/professional accent analysis, if you <i>really</i> want to
+          dive deep.
+        </li>
+      </ul>
+
+      <p className="mt-4">Accent training apps</p>
+      <ul className="px-8 list-disc">
+        <li>
+          <a
+            href="https://start.boldvoice.com/HVZQEF?d=R10&z=1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BoldVoice
+          </a>{" "}
+          (Free 7 day trial -&gt; monthly/yearly subscription)
         </li>
       </ul>
     </div>
