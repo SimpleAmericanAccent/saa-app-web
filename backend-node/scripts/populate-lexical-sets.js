0fdc467708a27fc6e92c1320f70d18878a20369f
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-04-28 22:51:09 -0500

    update database seeding scripts

--- backend-node/scripts/populate-lexical-sets.js
+++ backend-node/scripts/populate-lexical-sets.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const lexicalSets = [
@@ -13,94 +13,70 @@ const lexicalSets = [
     order: 2,
   },
   {
+    name: "TRAP",
+    description: "Words with the vowel sound in 'trap', 'cat', 'bad'",
+    order: 3,
+  },
+  {
     name: "DRESS",
     description: "Words with the vowel sound in 'dress', 'bed', 'head'",
-    order: 3,
+    order: 4,
+  },
+  {
+    name: "STRUT",
+    description: "Words with the vowel sound in 'strut', 'cup', 'love'",
+    order: 5,
+  },
+  {
+    name: "LOT",
+    description: "Words with the vowel sound in 'lot', 'odd', 'wash'",
+    order: 6,
   },
   {
     name: "FACE",
     description: "Words with the vowel sound in 'face', 'day', 'break'",
-    order: 4,
-  },
-  {
-    name: "PALM",
-    description: "Words with the vowel sound in 'palm', 'father', 'bra'",
-    order: 5,
-  },
-  {
-    name: "THOUGHT",
-    description: "Words with the vowel sound in 'thought', 'north', 'force'",
-    order: 6,
+    order: 7,
   },
   {
     name: "GOAT",
     description: "Words with the vowel sound in 'goat', 'show', 'no'",
-    order: 7,
+    order: 10,
   },
   {
     name: "FOOT",
     description: "Words with the vowel sound in 'foot', 'good', 'put'",
-    order: 8,
+    order: 11,
   },
   {
     name: "GOOSE",
     description: "Words with the vowel sound in 'goose', 'two', 'blue'",
-    order: 9,
+    order: 12,
   },
   {
     name: "PRICE",
-    description: "Words with the vowel sound in 'price', 'high', 'boy'",
-    order: 10,
+    description: "Words with the vowel sound in 'price', 'high', 'try'",
+    order: 13,
   },
   {
     name: "CHOICE",
     description: "Words with the vowel sound in 'choice', 'boy', 'oil'",
-    order: 11,
+    order: 14,
   },
   {
     name: "MOUTH",
     description: "Words with the vowel sound in 'mouth', 'now', 'out'",
-    order: 12,
-  },
-  {
-    name: "NEAR",
-    description: "Words with the vowel sound in 'near', 'here', 'weird'",
-    order: 13,
-  },
-  {
-    name: "SQUARE",
-    description: "Words with the vowel sound in 'square', 'fair', 'various'",
-    order: 14,
-  },
-  {
-    name: "START",
-    description: "Words with the vowel sound in 'start', 'arm', 'father'",
     order: 15,
-  },
-  {
-    name: "NORTH",
-    description: "Words with the vowel sound in 'north', 'force', 'horse'",
-    order: 16,
-  },
-  {
-    name: "FORCE",
-    description: "Words with the vowel sound in 'force', 'horse', 'hoarse'",
-    order: 17,
-  },
-  {
-    name: "CURE",
-    description: "Words with the vowel sound in 'cure', 'poor', 'jury'",
-    order: 18,
   },
   {
     name: "HAPPY",
     description: "Words with the vowel sound in 'happy', 'city', 'pretty'",
-    order: 19,
+    order: 22,
   },
   {
-    name: "LETTER",
-    description: "Words with the vowel sound in 'letter', 'about', 'comma'",
-    order: 20,
+    name: "COMMA",
+    description:
+      "Words with the reduced vowel sound in 'comma', 'about', 'the'",
+    order: 24,
   },
 ];
 
