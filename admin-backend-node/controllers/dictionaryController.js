d233e7c3e6cc52415940c42afd252cbb7ac404d2
Will <36839175+will-rosenberg@users.noreply.github.com>
2025-05-01 22:21:57 -0500

    trying to fix prisma

--- admin-backend-node/controllers/dictionaryController.js
+++ admin-backend-node/controllers/dictionaryController.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Get all dictionary entries with their usages
