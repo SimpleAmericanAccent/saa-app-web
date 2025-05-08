cb7f9b6db0abb8e837cdad45c5cdce647a9bc5dd
will-rosenberg <36839175+will-rosenberg@users.noreply.github.com>
2025-05-08 13:36:13 -0500

    refactor config.js into server.js

--- apps/user-backend-node/server.js
+++ apps/user-backend-node/server.js
@@ -1,31 +1,29 @@
-import url from "url";
 import path from "path";
-import {
-  environment_flag,
-  auth0Config,
-  AIRTABLE_BASE_ID,
-  AIRTABLE_KEY_READ_WRITE_VALUE,
-  AIRTABLE_KEY_READ_ONLY_VALUE,
-  DEFAULT_AUDIO_REC_ID,
-  AIRTABLE_KEY_SELECTED,
-} from "./config.js";
-import router from "core-backend-node/routes/routes.js";
+import url from "url";
 import { bootApp } from "core-backend-node/entry.js";
+import router from "core-backend-node/routes/routes.js";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 bootApp({
   dirname: __dirname,
-  environment_flag,
-  auth0Config,
+  environment_flag: process.env.ENVIRONMENT_FLAG,
+  auth0Config: {
+    authRequired: true,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET,
+    baseURL: process.env.AUTH0_BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_ID,
+  },
   router,
   frontendDir: "user-frontend-web",
   appLabel: "🙋 USER app",
   envConfig: {
-    AIRTABLE_BASE_ID,
-    AIRTABLE_KEY_READ_WRITE_VALUE,
-    AIRTABLE_KEY_READ_ONLY_VALUE,
-    DEFAULT_AUDIO_REC_ID,
-    AIRTABLE_KEY_SELECTED,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_KEY_READ_WRITE_VALUE: process.env.AIRTABLE_KEY_READ_WRITE_VALUE,
+    AIRTABLE_KEY_READ_ONLY_VALUE: process.env.AIRTABLE_KEY_READ_ONLY_VALUE,
+    DEFAULT_AUDIO_REC_ID: process.env.DEFAULT_AUDIO_REC_ID,
+    AIRTABLE_KEY_SELECTED: process.env.AIRTABLE_KEY_READ_ONLY_VALUE,
   },
 });
