5469fb4dedee50d91bdde852e016c57717cfd809
will-rosenberg <36839175+will-rosenberg@users.noreply.github.com>
2025-06-20 17:32:30 -0500

    posthog and portions of some other stuff to try to get native mobile working

--- packages/core-frontend-web/src/main.base.jsx
+++ packages/core-frontend-web/src/main.base.jsx
@@ -2,14 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.base.css";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24",
+};
 
 export function renderApp(AppComponent) {
   const root = document.getElementById("root");
   createRoot(root).render(
     <StrictMode>
-      <BrowserRouter>
-        <AppComponent />
-      </BrowserRouter>
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <BrowserRouter>
+          <AppComponent />
+        </BrowserRouter>
+      </PostHogProvider>
     </StrictMode>
   );
 }
